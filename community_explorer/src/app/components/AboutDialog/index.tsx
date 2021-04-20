8d642b66ab2624cbde55498bfb87623965d7a7d5
Steve Saylor <steven.saylor@pitt.edu>
2021-04-20 10:41:51 -0400

    added about page content

--- community_explorer/src/app/components/AboutDialog/index.tsx
+++ community_explorer/src/app/components/AboutDialog/index.tsx
@@ -30,36 +30,23 @@ export function AboutDialog(props: Props) {
   const { onClose } = props;
   return (
     <Dialog isDismissable onDismiss={onClose}>
-      <Heading>About this thing</Heading>
-      <Image
-        slot="hero"
-        src={isometricCityJPG}
-        alt="Isometric City Illustration"
-        objectFit="cover"
-        top={60}
-      ></Image>
+      <Heading>What is this?</Heading>
       <Divider />
       <Content>
         <Text>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            sem libero, tincidunt non sodales eu, cursus eget orci. Duis lacus
-            nibh, posuere eget bibendum id, faucibus et massa. Maecenas sodales
-            nisi non augue commodo, eu sodales ligula blandit. Sed non rhoncus
-            lacus, id rhoncus quam. Praesent sed orci non enim vestibulum
-            congue. Duis vel mauris eu lectus auctor faucibus. Duis accumsan
-            hendrerit feugiat. In a augue elementum, venenatis justo et, congue
-            purus. Aliquam eget arcu ac lectus feugiat sagittis. Pellentesque
-            vel eros tincidunt, rhoncus sapien vel, dapibus tortor. Integer
-            cursus neque purus, sed eleifend leo maximus id. Nunc congue, urna
-            sit amet placerat pulvinar, orci odio rutrum turpis, vel facilisis
-            magna est aliquet purus.{' '}
+            The Children's Health Data Explorer is designed to provide residents
+            of Allegheny County with a way to explore data in their communities
+            and help make sense of the information.{' '}
           </p>
-          <ul>
-            <li>Something</li>
-            <li>Another thing</li>
-            <li>I guess another thing if thats what folks want.</li>
-          </ul>
+          <p>
+            The Children’s Health Data Explorer presents community data and
+            indicators in a series of tables, charts and maps. Data comes from
+            local, state, and federal government sources. Data are available by
+            different geographies, from county and municipal levels down to
+            neighborhood and census tract and block groupings. Not all
+            information is available along all geographies, but much is.
+          </p>
         </Text>
         <Link
           variant="secondary"
